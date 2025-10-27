5f7389c395330780f8021ef4a1f380c2c7a414e0
ramires666 <grom.alexey@gmail.com>
2025-10-28 03:23:11 +0500

    05- home work

---
+++ 05-deployment/deployment/server.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+
+app = FastAPI()
+
+class Client(BaseModel):
+    lead_source: str
+    number_of_courses_viewed: int
+    annual_income: float
+
+pipeline_file = 'pipeline_v1.bin'
+with open(pipeline_file, 'rb') as f_in:
+    dict_vectorizer, model = pickle.load(f_in)
+
+@app.post("/predict")
+async def root(client_data: Client):
+    client_dict = client_data.dict()
+    X = dict_vectorizer.transform([client_dict])
+    result = model.predict_proba(X)[0, 1]
+    return result
